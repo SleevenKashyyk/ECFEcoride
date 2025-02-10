2940d070d90d36aa7c2b1426ac72f313bb5ba744
SleevenKashyyk <conte.ovin@gmail.com>
2025-02-10 10:34:59 +0100

    ajout inscription et connexion

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,11 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/vues", "Accès aux covoiturage", "/pages/vues.html"),
+    new Route("/connexion", "Connexion", "/pages/connexion.html"),
+    new Route("/inscription", "Inscription", "/pages/inscription.html"),
+
+
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
